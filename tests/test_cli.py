48e3a7edde2ee8da392285eba0ad4f9417c6305c
Stefan Hagen <stefan@hagen.link>
2020-07-20 08:46:47 +0200

    Create test_cli.py

---
+++ tests/test_cli.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import pytest  # type: ignore
+
+import zap_blank_lines.cli as cli
+
+
+def test_main_ok_string():
+    assert cli.main("imension is implicit") is None
+
+
+def test_main_ok_same_chars_in_string():
+    assert cli.main("a a a a") is None
+
+
+def test_main_nok_ints():
+    message = r"'int' object is not iterable"
+    with pytest.raises(TypeError, match=message):
+        cli.main([1, 2, 3])
+
+
+def test_main_nok_int():
+    message = r"'int' object has no attribute 'split'"
+    with pytest.raises(AttributeError, match=message):
+        cli.main(42)
